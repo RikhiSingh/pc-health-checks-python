ae6bc9639be6e90e8415f8852c6bafd0fa834809
My name <114336052+RikhiSingh@users.noreply.github.com>
2023-02-10 21:22:41 +0530

    copy pasted all_checks from checks folder and the refactor branch one(longer code)

---
+++ all_checks.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+import os
+import shutil
+import sys
+
+def check_reboot():
+	"""Returns True if the computer has a pending reboot"""
+	return os.path.exists("/run/reboot-required")
+
+def check_disk_full(disk, min_gb, min_percent):
+	"""Returns True if there isn't enough disk space, False otherwise."""
+	du = shutil.disk_usuage(disk)
+	# Calculate the percentage of free space
+	percent_free = 100 * du.free / du.total
+	# Calculate how many free gigabytes
+	gigabytes_free = du.free / 2**30
+	if gigabytes_fre < min_gb or percent_free < min_percent:
+		return True
+	return False
+
+def check_root_full():
+	"""Returns True if the root partition is full, False otherwise."""
+	return checkdisk_full(disk="/", min_gb = 2, min_percent = 10)
+
+def main():
+	checks=[
+		(check_reboot, "Pending reboot"),
+		(check_root_full), "Root partition full")'
+	]
+	everything_ok=True
+	for check, msg in checks:
+		if check():
+			print(msg)
+			everything_ok=False
+
+
+	if not everything_ok:
+		sys.exit(1)
+
+	print("Everything ok.")
+	sys.exit(0)
+main()
+
